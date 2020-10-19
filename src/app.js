f0f927721d36afd6e85c28c6c1567223de0a488c
Junshen Gao <57498746+Op1a4tzd@users.noreply.github.com>
2020-10-19 13:54:34 -0400

    a

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("connected");
+class Album {
+    contructor(title, artist, url){
+        this.title = title;
+        this.artist = artist;
+        this.url = url;
+    }
+}
+
+
+document.getElementById("form").addEventListener("submit", function (e) {
+
+    const title = document.getElementById("title").value;
+    const artist = document.getElementById("artist").value;
+    const url = document.getElementById("url").value;
+
+    const album = new Album(title, artist, url);
+    const ui
+}
+)
